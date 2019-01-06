8b3ad6e400f3dcd1f8599e33606c58e95a30aa8e
Nguyen Trung Thanh <thanhnt@portalbeanz.com>
2019-01-06 10:00:00 +0700

    Update service provider

--- src/Backpack2FAServiceProvider.php
+++ src/Backpack2FAServiceProvider.php
@@ -14,10 +14,8 @@ class Backpack2FAServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'laraviet');
         $this->loadViewsFrom(__DIR__ . '/resources/views', 'backpack2fa');
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-        // $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
 
         /*
         |--------------------------------------------------------------------------
@@ -35,11 +33,6 @@ class Backpack2FAServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/backpack2fa.php', 'backpack2fa');
-
-        // Register the service the package provides.
-        $this->app->singleton('backpack2fa', function ($app) {
-            return new Backpack2FA;
-        });
         $this->app['router']->aliasMiddleware('2fa', \PragmaRX\Google2FALaravel\Middleware::class);
     }
 
@@ -51,36 +44,5 @@ class Backpack2FAServiceProvider extends ServiceProvider
     public function provides()
     {
         return ['backpack2fa'];
-    }
-
-    /**
-     * Console-specific booting.
-     *
-     * @return void
-     */
-    protected function bootForConsole()
-    {
-        // Publishing the configuration file.
-        $this->publishes([
-            __DIR__ . '/../config/backpack2fa.php' => config_path('backpack2fa.php'),
-        ], 'backpack2fa.config');
-
-        // Publishing the views.
-        /*$this->publishes([
-            __DIR__.'/../resources/views' => base_path('resources/views/vendor/laraviet'),
-        ], 'backpack2fa.views');*/
-
-        // Publishing assets.
-        /*$this->publishes([
-            __DIR__.'/../resources/assets' => public_path('vendor/laraviet'),
-        ], 'backpack2fa.views');*/
-
-        // Publishing the translation files.
-        /*$this->publishes([
-            __DIR__.'/../resources/lang' => resource_path('lang/vendor/laraviet'),
-        ], 'backpack2fa.views');*/
-
-        // Registering package commands.
-        // $this->commands([]);
     }
 }
